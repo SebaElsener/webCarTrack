8fdb69afd380eae587ffa381918da3faa1779109
unknown <orichalcum5@hotmail.com>
2025-12-10 02:18:18 -0300

    secret

--- src/persistence/contenedores/contenedorFirebase.js
+++ src/persistence/contenedores/contenedorFirebase.js
@@ -5,13 +5,26 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+const serviceAccount = {
+"type": process.env.type,
+"project_id": process.env.project_id,
+"private_key_id": process.env.private_key_id,
+"private_key": process.env.private_key,
+"client_email": process.env.client_email,
+"client_id": process.env.client_id,
+"auth_uri": process.env.auth_uri,
+"token_uri": process.env.token_uri,
+"auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+"client_x509_cert_url": process.env.client_x509_cert_url
+}
+
 const require = createRequire(import.meta.url)
 //const serviceAccount = require('../config/coder-test-67523-firebase-adminsdk-w5t74-8bca7fec93.json')
 const admin = require("firebase-admin")
 
 try {
     admin.initializeApp({
-    credential: admin.credential.cert(process.env.serviceAccount)})
+    credential: admin.credential.cert(serviceAccount)})
     infoLogger.info('FIREBASE:  Base de datos conectada')
 } catch (error) {
     infoLogger.info('ERROR DE CONEXION A FIREBASE', error)
